24d917d14efe44254c6a1a088afc2377ea58e7c6
sealer24 <44804342+sealer24@users.noreply.github.com>
2018-12-11 15:38:42 +0100

    first

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head>
+        <title>Portfolio</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="">
+    </head>
+    <body id="pagewrap">
+        <div id="phpheader">
+        <h1 id="overskrift" >overskriften</h1>
+        <img id="mmdbillede" src="mmd billede" alt="billede af mmd">
+        </div>
+        <p id="overordnet tekst" class="tekst">overordnet tekst: Lorem ipsum dolor sit amet, aliquam ut orci montes eget felis eleifend, sociosqu in. Nisl posuere sed lacinia quam praesent. Hac donec, praesent vel mauris, mollis ut mollis, gravida gravida aliquid nec quam ut lacus. Ante leo magna, pellentesque in turpis sed posuere in, rutrum ultricies aliquam sit potenti, a donec magnam curabitur curabitur nisl in. Posuere etiam tellus venenatis cupidatat. Vulputate pellentesque scelerisque dictumst diam nec adipiscing, amet elit, id amet vestibulum ornare sed. Vehicula natoque amet libero libero, nulla malesuada. Aliquet tempor.</p>
+        <div id="container1">
+            <p id="teksttools" class="tekst">Webudvikling tekst: Arcu luctus imperdiet eu morbi quis convallis. Montes id nunc wisi, condimentum convallis, aliquam suscipit consectetuer ipsam leo auctor phasellus, vestibulum ac. Aliquet cum sodales eu non neque metus.</p>
+            <img id="billedeweb" class="billede" src="CSS.jpg" alt="billede web">
+        </div>
+        <div id="container2">
+            <p id="tekstkom" class="tekst">Webudvikling tekst: Arcu luctus imperdiet eu morbi quis convallis. Montes id nunc wisi, condimentum convallis, aliquam suscipit consectetuer ipsam leo auctor phasellus, vestibulum ac. Aliquet cum sodales eu non neque metus.</p>
+            <img id="billedeweb" class="billede" src="CSS.jpg" alt="billede web">
+        </div>
+        <div id="container3">
+            <p id="tekstmetoder" class="tekst">Webudvikling tekst: Arcu luctus imperdiet eu morbi quis convallis. Montes id nunc wisi, condimentum convallis, aliquam suscipit consectetuer ipsam leo auctor phasellus, vestibulum ac. Aliquet cum sodales eu non neque metus.</p>
+            <img id="billedeweb" class="billede" src="CSS.jpg" alt="billede web">
+        </div>
+        <div id="container4">
+            <p id="tekstdesign" class="tekst">Webudvikling tekst: Arcu luctus imperdiet eu morbi quis convallis. Montes id nunc wisi, condimentum convallis, aliquam suscipit consectetuer ipsam leo auctor phasellus, vestibulum ac. Aliquet cum sodales eu non neque metus.</p>
+            <img id="billedeweb" class="billede" src="CSS.jpg" alt="billede web">
+        </div>
+        <div id="container5">
+            <p id="tekstprojekt" class="tekst">Webudvikling tekst: Arcu luctus imperdiet eu morbi quis convallis. Montes id nunc wisi, condimentum convallis, aliquam suscipit consectetuer ipsam leo auctor phasellus, vestibulum ac. Aliquet cum sodales eu non neque metus.</p>
+            <img id="billedeweb" class="billede" src="CSS.jpg" alt="billede web">
+        </div>
+        <div id="container6">
+            <p id="tekstref" class="tekst">Webudvikling tekst: Arcu luctus imperdiet eu morbi quis convallis. Montes id nunc wisi, condimentum convallis, aliquam suscipit consectetuer ipsam leo auctor phasellus, vestibulum ac. Aliquet cum sodales eu non neque metus.</p>
+            <img id="billedeweb" class="billede" src="CSS.jpg" alt="billede web">
+        </div>
+        
+    </body>
